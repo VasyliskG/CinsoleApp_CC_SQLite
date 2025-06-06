14d3ab317efb909d3b27d328a8481f8b867a892e
VasyliskG <c.kozak.vasyl@student.uzhnu.edu.ua>
2025-06-06 21:03:53 +0300

    feat(menu): add interactive console menu using Spectre.Console

---
+++ CinsoleApp_CC_SQLite/Program.cs
@@ -0,0 +1,42 @@
+﻿using Spectre.Console;
+using System.Data.SQLite;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            var option = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Вас вітає консольна програма для роботи з SQLite оберіть [green]дію[/]:")
+                    .PageSize(10)
+                    .AddChoices(new[] {
+                        "Перегляд списку користувачів",
+                        "Перегляд списку продуктів",
+                        "Додавання нового користувача",
+                        "Вихід із програми"
+                    }));
+
+            switch (option)
+            {
+                case "Перегляд списку користувачів":
+                    
+                    break;
+                case "Перегляд списку продуктів":
+                    
+                    break;
+                case "Додавання нового користувача":
+                    
+                    break;
+                case "Вихід із програми":
+                    AnsiConsole.MarkupLine("[red]До побачення![/]");
+                    return;
+            }
+
+            AnsiConsole.MarkupLine("[grey]Натисніть будь-яку клавішу для продовження...[/]");
+            Console.ReadKey(true);
+            AnsiConsole.Clear();
+        }
+    }
+}
